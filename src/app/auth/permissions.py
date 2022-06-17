4f658223c840370763471ce1fca9db8133bd97c4
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-06-17 23:29:04 +0300

    Rename get_user permission to get_current_active_user

--- src/app/auth/permissions.py
+++ src/app/auth/permissions.py
@@ -31,7 +31,7 @@ def get_current_user(token: str = Security(reusable_oauth2), session: Session = 
     return user
 
 
-def get_user(current_user: User = Security(get_current_user)) -> User:
+def get_current_active_user(current_user: User = Security(get_current_user)) -> User:
     if not current_user.is_active:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
--- src/app/user/endpoints.py
+++ src/app/user/endpoints.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from auth.permissions import get_user
+from auth.permissions import get_current_active_user
 from db.models.user import User
 from user.schemas import BaseUser
 
@@ -8,5 +8,5 @@ user_router = APIRouter()
 
 
 @user_router.get("/me", response_model=BaseUser)
-def get_current_user(user: User = Depends(get_user)):
+def get_current_user(user: User = Depends(get_current_active_user)):
     return BaseUser.from_orm(user)
