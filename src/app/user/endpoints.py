e762fd783a00de4a6b0da565d1fee416e478581d
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-06-23 19:36:42 +0300

    Delete comment and unused import

--- src/app/user/endpoints.py
+++ src/app/user/endpoints.py
@@ -5,7 +5,6 @@ from auth.permissions import get_current_active_user
 from db.models.user import User
 from db.session import get_session
 from user.schemas import UserRetrieve, UserUpdate
-from schemas import Message
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -20,13 +19,11 @@ def get_current_user(user: User = Depends(get_current_active_user)):
 def update_current_user(
     user_to_update: UserUpdate,
     current_user: User = Depends(get_current_active_user),
-    session: Session = Depends(get_session)
+    session: Session = Depends(get_session),
 ):
     user = crud.update_object(
         session=session,
         object_=current_user,
         to_update=user_to_update.dict(exclude_none=True),
     )
-    # return Message(message="You've updated your user details.")
-    return user
-
+    return UserRetrieve.from_orm(user)
