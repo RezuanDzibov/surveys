0d0b8d6b6365fb18f3a4269d2525ec4077072975
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-07-21 20:51:10 +0300

    tests: add jwt test

---
+++ src/app/tests/test_jwt.py
@@ -0,0 +1,23 @@
+import jwt
+
+from auth import jwt as auth_jwt
+from settings import get_settings
+
+settings = get_settings()
+
+
+def test_create_token(admin_user, test_client):
+    token = auth_jwt.create_acess_token(user_id=str(admin_user.get("id")))
+    response = test_client.get(
+        "user/me",
+        headers={
+            "Authorization": f"Bearer {token.get('access_token')}"
+        }
+    )
+    assert response.status_code == 200
+
+
+def test_create_jwt_token(admin_user):
+    token = auth_jwt.create_acess_token(user_id=str(admin_user.get("id"))).get("access_token")
+    decoded_access_token = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.TOKEN_ENCODE_ALGORITHM])
+    assert decoded_access_token.get("user_id") == str(admin_user.get("id"))
