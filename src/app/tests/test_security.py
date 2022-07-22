a88e7acbec47957ebf70fdc72e5bc399d8dfacd3
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-07-22 10:24:00 +0300

    tests: add test_security

---
+++ src/app/tests/test_security.py
@@ -0,0 +1,6 @@
+from auth import security
+
+
+def test_password_hashing():
+    hashed_password = security.get_password_hash(password="password")
+    assert security.verify_password(plain_password="password", hashed_password=hashed_password)
