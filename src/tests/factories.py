c9c149adcfa6995962b01c33ec16c21f94388ee7
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-07-27 14:55:24 +0300

    fix: wrong import

--- src/tests/factories.py
+++ src/tests/factories.py
@@ -3,8 +3,8 @@ from random import choice
 import factory
 from faker import Faker
 
-from auth.security import get_password_hash
-from db.models import User
+from app.auth.security import get_password_hash
+from app.db.models import User
 
 fake = Faker()
 
