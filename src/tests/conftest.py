33beaf39c617aaba715bdd3eac3fb3d3ebdc5ec1
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-07-27 14:48:43 +0300

    refactor, feature: refactor conftest.py and add factory_users fixture

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,24 +3,30 @@ from unittest import mock
 
 import pytest
 from fastapi.testclient import TestClient
+from pytest_factoryboy import register
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import sessionmaker, Session
 
 from app.db.models import Base
 from app.initial_data_fixtures import create_admin_user
 from app.main import app
 from app.settings import get_settings
+from db.models import User
+from tests.factories import UserFactory
 
 settings = get_settings()
 
+register(UserFactory)
+
 
 @pytest.fixture(scope="session")
-def engine():
+def engine() -> Engine:
     return create_engine(settings.SQLALCHEMY_DATABASE_URI)
 
 
 @pytest.fixture(scope="function")
-def tables(engine):
+def tables(engine) -> None:
     Base.metadata.create_all(engine)
     try:
         yield
@@ -34,7 +40,7 @@ def session_maker(engine):
 
 
 @pytest.fixture(scope="function")
-def session(tables, session_maker):
+def session(tables, session_maker) -> Session:
     session = session_maker()
     try:
         yield session
@@ -43,13 +49,13 @@ def session(tables, session_maker):
 
 
 @pytest.fixture(scope="function")
-def admin_user(request, session):
+def admin_user(request, session) -> dict:
     create_admin_user_ = partial(create_admin_user, session)
     return create_admin_user_(data_to_replace=request.param) if hasattr(request, "param") else create_admin_user_()
 
 
 @pytest.fixture(scope="function")
-def admin_user_data():
+def admin_user_data() -> dict:
     return {
         "username": settings.ADMIN_FIXTURE_USERNAME,
         "email": settings.ADMIN_FIXTURE_EMAIL,
@@ -64,12 +70,22 @@ def admin_user_data():
 
 
 @pytest.fixture(scope="function")
-def task():
+def task() -> mock.Mock:
     task = mock.Mock()
     task.add_task = mock.MagicMock()
     return task
 
 
 @pytest.fixture(scope="function")
-def test_client():
+def test_client() -> TestClient:
     return TestClient(app)
+
+
+@pytest.fixture(scope="function")
+def factory_users(request, session: Session, user_factory: UserFactory) -> list[User]:
+    if request.param:
+        users: [User] = user_factory.build_batch(request.param)
+        session.add_all(users)
+        session.commit()
+        return users
+    return user_factory.create()
