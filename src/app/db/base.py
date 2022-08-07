52ef14a2d32dcb03cc5caba2daad5deb4aed09a9
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-08-07 20:23:16 +0300

    make get_session async

--- src/app/db/base.py
+++ src/app/db/base.py
@@ -1,18 +1,15 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 from core.settings import get_settings
 
 settings = get_settings()
 
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 
 
-def get_session() -> Session:
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def get_session() -> AsyncSession:
+    async with SessionLocal() as session:
+        yield session
