ef17519230fbea42e167070eee3134123fd5c1fb
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-08-08 13:04:57 +0300

    add get_user endpoint and fix type hint in update_current_user

--- src/app/api/endpoints/user.py
+++ src/app/api/endpoints/user.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
+from pydantic import UUID4
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.deps import get_current_active_user
@@ -20,7 +21,7 @@ async def get_current_user(user: User = Depends(get_current_active_user)):
 @router.patch("/me/update", response_model=user_schemas.UserRetrieve)
 async def update_current_user(
     user_to_update: user_schemas.UserUpdate,
-    current_user: dict = Depends(get_current_active_user),
+    current_user: User = Depends(get_current_active_user),
     session: AsyncSession = Depends(get_session),
 ):
     user = await user_services.update_user(
@@ -35,3 +36,9 @@ async def update_current_user(
 async def get_users(session: AsyncSession = Depends(get_session)):
     users = await user_services.get_users(session=session)
     return users
+
+
+@router.get("/{user_id}", response_model=user_schemas.UserRetrieve)
+async def get_user(user_id: UUID4, session: AsyncSession = Depends(get_session)):
+    user = await user_services.get_user(session=session, where_statements=[User.id == user_id])
+    return user
