330634354951dcd480b27f4f961cdcc290a5e988
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-08-21 20:58:14 +0300

    add survey_router

--- src/app/api/routes.py
+++ src/app/api/routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from api.endpoints.auth import router as auth_router
+from api.endpoints.survey import router as survey_router
 from api.endpoints.user import router as user_router
 
 router = APIRouter()
 
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
-router.include_router(user_router, prefix="/users", tags=["users"])
+router.include_router(user_router, prefix="/user", tags=["user"])
+router.include_router(survey_router, prefix="/survey", tags=["survey"])
