856e25a3e2672380d2e6ce68e186a81ea1aac63a
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-08-27 15:08:53 +0300

    delete utils.py

--- src/app/db/utils.py
+++
@@ -1,6 +0,0 @@
-def orm_row_to_dict(object_) -> dict:
-    object_ = object_[0]
-    to_return = dict()
-    for column in object_.__table__.columns:
-        to_return[column.name] = getattr(object_, column.name)
-    return to_return
