2e4dbb1a2ce74ee007f6e2d7d534873c66de8b5f
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-09-02 13:46:41 +0300

    refactor TestCreateSurvey

--- src/tests/test_services/test_survey.py
+++ src/tests/test_services/test_survey.py
@@ -1,9 +1,13 @@
+import uuid
+from typing import List
+
+import pytest
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models import User
-from schemas.survey import SurveyCreate, SurveyBase
+from models import User, SurveyAttribute
+from schemas.survey import SurveyCreate, SurveyBase, Survey
 from services import survey as survey_services
-from tests.factories import SurveyAttributeFactory
 
 
 class TestCreateSurvey:
@@ -11,10 +15,10 @@ class TestCreateSurvey:
             self,
             admin_user: User,
             session: AsyncSession,
+            build_survey_attrs: List[SurveyAttribute]
     ):
         survey_data = {"name": "name"}
-        attrs = SurveyAttributeFactory.build_batch(3)
-        attrs = [attr.as_dict() for attr in attrs]
+        attrs = list(attr.as_dict() for attr in build_survey_attrs)
         survey = SurveyCreate(**survey_data, attrs=attrs)
         survey = await survey_services.create_survey(session=session, survey=survey, user_id=admin_user.id)
         assert SurveyBase(**survey_data) == SurveyBase(**survey.as_dict())
