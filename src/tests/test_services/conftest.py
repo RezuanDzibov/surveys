9faf6e9b046ebb6a4bcb31710bd091661a7f33ce
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-09-02 13:45:04 +0300

    add factory_surveys

--- src/tests/test_services/conftest.py
+++ src/tests/test_services/conftest.py
@@ -10,6 +10,7 @@ from services.survey import _create_survey_attributes
 from tests.factories import SurveyAttributeFactory, SurveyFactory
 
 register(SurveyAttributeFactory)
+register(SurveyFactory)
 
 
 @pytest.fixture(scope="function")
@@ -23,3 +24,35 @@ async def build_survey_attrs(
         return attrs
     attr = survey_attribute_factory.build()
     return attr
+
+
+@pytest.fixture(scope="function")
+async def factory_surveys(
+        request: SubRequest,
+        session: AsyncSession,
+        admin_user: User,
+        survey_factory: SurveyFactory,
+        survey_attribute_factory: SurveyAttributeFactory
+) -> Union[Survey, List[Survey]]:
+    attrs = survey_attribute_factory.build_batch(3)
+    attrs = list([attr.as_dict() for attr in attrs])
+    if hasattr(request, "param"):
+        surveys = survey_factory.build_batch(request.param)
+        for survey in surveys:
+            survey.user_id = str(admin_user.id)
+        session.add_all(surveys)
+        await session.commit()
+        for survey in surveys:
+            await session.refresh(survey)
+            await _create_survey_attributes(session=session, survey_id=str(survey.id), attrs=attrs)
+        for survey in surveys:
+            await session.refresh(survey)
+        return surveys
+    survey = survey_factory.build()
+    survey.user_id = str(admin_user.id)
+    session.add(survey)
+    await session.commit()
+    await session.refresh(survey)
+    await _create_survey_attributes(session=session, survey_id=str(survey.id), attrs=attrs)
+    await session.refresh(survey)
+    return survey
