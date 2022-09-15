8f15fc8b441645169eb94c9a0e0a299919815be7
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-09-15 20:50:53 +0300

    add update_survey and update_survey_attr endpoints

--- src/app/api/endpoints/survey.py
+++ src/app/api/endpoints/survey.py
@@ -4,10 +4,10 @@ from fastapi import APIRouter, Depends
 from pydantic import UUID4
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.deps import get_current_user
+from app.api.deps import get_current_user, get_current_active_user
 from app.db.base import get_session
 from app.models import User
-from app.schemas.survey import SurveyCreate, SurveyOut, SurveyRetrieve
+from app.schemas.survey import SurveyCreate, SurveyOut, SurveyRetrieve, SurveyUpdate, SurveyAttributeUpdate
 from app.services import survey as survey_services
 
 router = APIRouter()
@@ -36,3 +36,30 @@ async def get_survey(
 async def get_surveys(available: Optional[bool] = None, session: AsyncSession = Depends(get_session)):
     surveys = await survey_services.get_surveys(session=session, available=available)
     return surveys
+
+
+@router.patch("/{id_}")
+async def update_survey(
+        id_: UUID4,
+        to_update: SurveyUpdate,
+        session: AsyncSession = Depends(get_session),
+        current_user: User = Depends(get_current_active_user)
+):
+    survey = await survey_services.update_survey(session=session, user=current_user, id_=id_, to_update=to_update)
+    return survey
+
+
+@router.patch("/attr/{id_}")
+async def update_survey_attr(
+        id_: UUID4,
+        to_update: SurveyAttributeUpdate,
+        session: AsyncSession = Depends(get_session),
+        current_user: User = Depends(get_current_active_user),
+):
+    survey_attr = await survey_services.update_survey_attribute(
+        session=session,
+        user=current_user,
+        id_=id_,
+        to_update=to_update
+    )
+    return survey_attr
