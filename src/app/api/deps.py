170439bdebf677a90a62e0540f1fd1b70fe5b444
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-09-20 19:44:36 +0300

    add get_current_active_user_or_none fastapi dep

--- src/app/api/deps.py
+++ src/app/api/deps.py
@@ -1,19 +1,18 @@
-import jwt
+from typing import Optional
+
 from fastapi import Depends, HTTPException, Security
 from fastapi.security import OAuth2PasswordBearer
-from jwt import PyJWTError
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.status import HTTP_403_FORBIDDEN
 
+from app.core.jwt import get_user_by_jwt_token
 from app.core.settings import get_settings
 from app.db.base import get_session
 from app.models.user import User
-from app.schemas.auth import TokenPayload
-from app.services import user as user_services
 
 settings = get_settings()
 
 reusable_oauth2 = OAuth2PasswordBearer(tokenUrl="/login/access-token")
+optional_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/access-token", auto_error=False)
 
 
 async def get_current_user(token: str = Security(reusable_oauth2), session: AsyncSession = Depends(get_session)) -> User:
@@ -40,3 +39,12 @@ async def get_superuser(current_user: User = Security(get_current_active_user)) 
             status_code=400, detail="The user doesn't have enough privileges",
         )
     return current_user
+
+
+async def get_current_active_user_or_none(
+        token: Optional[str] = Security(optional_oauth2_scheme),
+        session: AsyncSession = Depends(get_session)
+) -> Optional[User]:
+    if token:
+        return await get_user_by_jwt_token(token=token, session=session)
+    return None
