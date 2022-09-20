7169378d23528ed4bc9b0e9dbaa11f05a92683c7
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-09-20 19:45:54 +0300

    refactor get_current_user dep, user get_user_by_jwt_token to get user

--- src/app/api/deps.py
+++ src/app/api/deps.py
@@ -15,16 +15,11 @@ reusable_oauth2 = OAuth2PasswordBearer(tokenUrl="/login/access-token")
 optional_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/access-token", auto_error=False)
 
 
-async def get_current_user(token: str = Security(reusable_oauth2), session: AsyncSession = Depends(get_session)) -> User:
-    try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.TOKEN_ENCODE_ALGORITHM])
-        token_data = TokenPayload(**payload)
-    except PyJWTError:
-        raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials.",
-        )
-    user = await user_services.get_user(session=session, where_statements=[User.id == token_data.user_id])
-    return user
+async def get_current_user(
+        token: str = Security(reusable_oauth2),
+        session: AsyncSession = Depends(get_session)
+) -> User:
+    return await get_user_by_jwt_token(token=token, session=session)
 
 
 async def get_current_active_user(current_user: User = Security(get_current_user)) -> User:
