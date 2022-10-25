66a2e16e7dbed80fe77606a4676b0b14a416259a
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-10-25 21:54:02 +0300

    add answer router

--- src/app/api/routes.py
+++ src/app/api/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.api.endpoints.answer import router as answer_router
 from app.api.endpoints.auth import router as auth_router
 from app.api.endpoints.survey import router as survey_router
 from app.api.endpoints.user import router as user_router
@@ -9,3 +10,4 @@ router = APIRouter()
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
 router.include_router(user_router, prefix="/user", tags=["user"])
 router.include_router(survey_router, prefix="/survey", tags=["survey"])
+router.include_router(answer_router, prefix="/answer", tags=["answer"])
