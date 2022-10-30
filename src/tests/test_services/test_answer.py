1f1fb26ba71ef64021f66455cf694c080f0f18f2
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-10-30 22:40:39 +0300

    add TestDeleteAnswer test to test_services

--- src/tests/test_services/test_answer.py
+++ src/tests/test_services/test_answer.py
@@ -3,10 +3,12 @@ from uuid import uuid4
 
 import pytest
 from fastapi import HTTPException
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import AnswerAttribute, User, Survey, Answer
 from app.schemas import survey as schemas
+from app.services import base as base_services
 from app.services import survey as survey_services
 from tests.factories import AnswerAttributeFactory
 from tests.utils import build_answer_attrs_with_survey_attrs
@@ -113,3 +115,44 @@ class TestCreateAnswerAttributes:
         )
         assert expected_attrs == [schemas.AnswerAttribute.from_orm(attr) for attr in created_attrs]
 
+
+class TestDeleteAnswer:
+    @pytest.mark.parametrize("factory_answer", [True], indirect=True)
+    async def test_success(self, session: AsyncSession, admin_user: User, factory_answer: Answer):
+        await survey_services.delete_answer(
+            session=session,
+            user=admin_user,
+            answer_id=factory_answer.id
+
+        )
+        assert not await base_services.is_object_exists(
+            session=session,
+            where_statement=select(Answer).where(Answer.id == factory_answer.id)
+        )
+        survey_attr_ids = [answer_attr.id for answer_attr in factory_answer.attrs]
+        statement = select(AnswerAttribute).where(
+            AnswerAttribute.id.in_(
+                survey_attr_ids
+            )
+        )
+        result = await session.execute(statement)
+        assert not result.scalars().all()
+
+    async def test_404(self, session: AsyncSession, admin_user: User):
+        with pytest.raises(HTTPException) as exception:
+            await survey_services.delete_answer(
+                session=session,
+                user=admin_user,
+                answer_id=uuid4()
+            )
+            assert exception.value.status_code == 404
+
+    @pytest.mark.parametrize("factory_answer", [True], indirect=True)
+    async def test_for_not_author(self, session: AsyncSession, factory_answer: Answer, user_and_its_pass: dict):
+        with pytest.raises(HTTPException) as exception:
+            await survey_services.delete_answer(
+                session=session,
+                user=user_and_its_pass["user"],
+                answer_id=uuid4()
+            )
+            assert exception.value.status_code == 403
