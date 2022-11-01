3e2c2252571cbfc1b9adcb9c62e8fa7b4d10ba7a
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-01 15:42:05 +0300

    use build_answer pytest fixture in test_answer.py in endpoint tests

--- src/tests/test_endpoints/test_answer.py
+++ src/tests/test_endpoints/test_answer.py
@@ -21,11 +21,10 @@ class TestAddAnswer:
     async def test_success(
             self,
             auth_test_client: AsyncClient,
-            factory_survey: Survey
-
+            factory_survey: Survey,
+            build_answer: Answer
     ):
-        attrs = await build_answer_attrs_with_survey_attrs(survey=factory_survey)
-        expected_answer = await serialize_uuid_to_str(schemas.BaseAnswer(attrs=attrs).dict())
+        expected_answer = await serialize_uuid_to_str(schemas.BaseAnswer.from_orm(build_answer).dict())
         response = await auth_test_client.post(f"/answer/{factory_survey.id}", json=expected_answer)
         created_answer = json.loads(response.content.decode("utf-8"))
         assert expected_answer == json.loads(json.dumps(schemas.BaseAnswer(**created_answer).dict(), default=uuid_to_str))
@@ -33,14 +32,14 @@ class TestAddAnswer:
     async def test_not_exist_survey_attr(
             self,
             auth_test_client: AsyncClient,
-            factory_survey: Survey
+            factory_survey: Survey,
+            build_answer: Answer
 
     ):
-        attrs = await build_answer_attrs_with_survey_attrs(survey=factory_survey)
         attr = AnswerAttributeFactory.build()
-        attr.survey_attr_id = str(uuid4())
-        attrs.append(attr.as_dict())
-        answer = await serialize_uuid_to_str(schemas.BaseAnswer(attrs=attrs).dict())
+        attr.survey_attr_id = uuid4()
+        build_answer.attrs.append(attr)
+        answer = await serialize_uuid_to_str(schemas.BaseAnswer.from_orm(build_answer).dict())
         response = await auth_test_client.post(f"/answer/{factory_survey.id}", json=answer)
         assert response.status_code == 404
 
@@ -48,20 +47,20 @@ class TestAddAnswer:
             self,
             auth_test_client: AsyncClient,
             factory_survey: Survey,
-            factory_answer: Answer
+            factory_answer: Answer,
+            build_answer: Answer
     ):
-        attrs = await build_answer_attrs_with_survey_attrs(survey=factory_survey)
-        answer = await serialize_uuid_to_str(schemas.BaseAnswer(attrs=attrs).dict())
+        answer = await serialize_uuid_to_str(schemas.BaseAnswer.from_orm(build_answer).dict())
         response = await auth_test_client.post(f"/answer/{factory_answer.survey_id}", json=answer)
         assert response.status_code == 409
 
     async def test_not_exists_survey(
             self,
             auth_test_client: AsyncClient,
-            factory_survey: Survey
+            factory_survey: Survey,
+            build_answer: Answer
     ):
-        attrs = await build_answer_attrs_with_survey_attrs(survey=factory_survey)
-        answer = await serialize_uuid_to_str(schemas.BaseAnswer(attrs=attrs).dict())
+        answer = await serialize_uuid_to_str(schemas.BaseAnswer.from_orm(build_answer).dict())
         response = await auth_test_client.post(f"/answer/{uuid4()}", json=answer)
         assert response.status_code == 404
 
@@ -69,9 +68,9 @@ class TestAddAnswer:
             self,
             factory_survey: Survey,
             test_client: AsyncClient,
+            build_answer: Answer
     ):
-        attrs = await build_answer_attrs_with_survey_attrs(survey=factory_survey)
-        answer = await serialize_uuid_to_str(schemas.BaseAnswer(attrs=attrs).dict())
+        answer = await serialize_uuid_to_str(schemas.BaseAnswer.from_orm(build_answer).dict())
         response = await test_client.post(f"/answer/{factory_survey.id}", json=answer)
         assert response.status_code == 401
 
