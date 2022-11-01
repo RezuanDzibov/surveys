d6b0b97328d2dc0e4a78abe6ef568fc7d4c145cc
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-01 15:25:53 +0300

    add AnswerAttributeRetrieve, AnswerRetrieve pydantic models

--- src/app/schemas/survey.py
+++ src/app/schemas/survey.py
@@ -89,3 +89,21 @@ class BaseAnswer(BaseModel):
 
 class AnswerCreateOut(BaseAnswer):
     id: UUID4
+
+
+class AnswerAttributeRetrieve(BaseModel):
+    id: UUID4
+    text: constr(max_length=255)
+    survey_attr_id: UUID4
+
+    class Config:
+        orm_mode = True
+
+
+class AnswerRetrieve(BaseModel):
+    id: UUID4
+    available: bool
+    attrs: List[AnswerAttributeRetrieve]
+
+    class Config:
+        orm_mode = True
